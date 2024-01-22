b69c65626c4caba2b75bdd6f204d296029c5a809
Josan Paier <131397521+josanpaier@users.noreply.github.com>
2024-01-22 17:15:46 -0300

    Add files via upload

---
+++ ProgramaPernas/src/programapernas/ProgramaPernas.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package programapernas;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author josan
+ */
+public class ProgramaPernas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Quantas pernas?");
+        int perna = teclado.nextInt();
+        String tipo;
+        System.out.print("Isso é um(a): ");
+        switch (perna) {
+            case 1:
+                tipo = "Saci";
+                break;
+            case 2:
+                tipo = "Bípede";
+                break;
+            case 4:
+                tipo = "Quadrúpede";
+                break;
+            case 6, 8:
+                tipo = "Aranha";
+                break;
+            default:
+                tipo = "ET";                
+        }
+        System.out.println(tipo);
+    }
+    
+}
